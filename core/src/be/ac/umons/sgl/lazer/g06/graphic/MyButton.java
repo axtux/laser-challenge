d66226728af271c7b7546e01ee9179bf727db887
Axtux <axtux@hotmail.com>
2017-03-01 14:29:27 +0100

    correct indentation and separate packfile from textures

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/MyButton.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/MyButton.java
@@ -1,52 +1,51 @@
 package be.ac.umons.sgl.lazer.g06.graphic;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+/* Textures from packfile
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+//*/
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class MyButton {
- TextButton button;
- TextButtonStyle textButtonStyle;
- BitmapFont font;
- Skin skin;
- TextureAtlas buttonAtlas;
+	TextButton button;
+	TextButtonStyle textButtonStyle;
+	
+	public MyButton(String text) {
+		// default 15pt Arial
+		BitmapFont font = new BitmapFont();
 
- public MyButton(String text) {
-	 font = new BitmapFont();
-	/*
-	AssetManager am = new AssetManager();
-	am.load("buttons.pack", TextureAtlas.class);
-	am.finishLoading();
-	buttonAtlas = am.get("buttons.pack");
-	//*/
-	 //buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
-	 //skin.addRegions(buttonAtlas);
-	 Texture tex = new Texture("images/MainMenu/connexion_anonyme_button_on.png");
-	 TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(tex));
-	 
-	 textButtonStyle = new TextButtonStyle();
-	 textButtonStyle.font = font;
-	 //*
-	 textButtonStyle.up = drawable;
-	 textButtonStyle.down = drawable;
-	 textButtonStyle.checked = drawable;
-	 //*/
-	 /*
-	 textButtonStyle.up = skin.getDrawable("up-button");
-	 textButtonStyle.down = skin.getDrawable("down-button");
-	 textButtonStyle.checked = skin.getDrawable("checked-button");
-	 //*/
-	 button = new TextButton(text, textButtonStyle);
- }
- 
- public TextButton getButton() {
-	 return button;
- }
+		/* Textures from packfile
+		TextureAtlas buttonAtlas = new TextureAtlas("relative/from/assets/to/file.pack");
+		Skin skin.addRegions(buttonAtlas);
+		//*/
+		//* Individual textures
+		Texture tex = new Texture("images/MainMenu/connexion_anonyme_button_on.png");
+		TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(tex));
+		//*/
+		
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = font;
+
+		/* Textures from packfile
+		textButtonStyle.up = skin.getDrawable("up-button");
+		textButtonStyle.down = skin.getDrawable("down-button");
+		textButtonStyle.checked = skin.getDrawable("checked-button");
+		//*/
+		//* Individual textures
+		textButtonStyle.up = drawable;
+		textButtonStyle.down = drawable;
+		textButtonStyle.checked = drawable;
+		//*/
+		
+		button = new TextButton(text, textButtonStyle);
+	}
+	
+	public TextButton getButton() {
+		return button;
+	}
 }
