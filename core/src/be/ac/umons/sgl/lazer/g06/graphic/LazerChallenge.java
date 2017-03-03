527748a369f6b4abb7da6d253121749df55ece2a
Axtux <axtux@hotmail.com>
2017-03-03 13:35:48 +0100

    code cleaning

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
@@ -2,36 +2,73 @@ package be.ac.umons.sgl.lazer.g06.graphic;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
 import be.ac.umons.sgl.lazer.g06.listeners.ButtonListener;
 
 public class LazerChallenge extends Game {
-	SpriteBatch batch;
+	Batch batch;
 	public static int WIDTH=1000;
 	public static int HEIGHT=720;
 	Stage stage;
+	Table table;
+	MySkin skin;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		//batch = new SpriteBatch();
 		//this.setScreen(new MainMenu(this));
 		//*
 		stage = new Stage();
 		Gdx.input.setInputProcessor(stage);
 		
-		MyButton btn = new MyButton("Coucou petite perruche !");
-		ButtonListener bl = new ButtonListener("MENU");
-		btn.addListener(bl);
-		stage.addActor(btn);
+		batch = stage.getBatch();
+		
+		table = new Table();
+		stage.addActor(table);
+		table.setFillParent(true);
+		//table.setDebug(true);
+		
+		skin = new MySkin();
+		table.setBackground(skin.getColor(Color.WHITE));
+		
+		MyButton btn = new MyButton(skin, "Coucou petite perruche !");
+		btn.addListener(new ButtonListener("MENU"));
+		table.add(btn).minSize(800, 80);
+		table.row();
+		MyButton btn2 = new MyButton(skin, "C'est moi l'élan");
+		btn2.addListener(new ButtonListener("MENU2"));
+		table.add(btn2).minSize(800, 80);
+		
+		table.row();
 		//stage.add(batch);
 		//stage.addActor(actor);
 		//*/
 	}
 	
+	public void resize (int width, int height) {
+		stage.getViewport().update(width, height, true);
+	}
+	
 	public void render(){
 		super.render();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
+	}
+	
+	public void dispose() {
+		stage.dispose();
 	}
 }
--- core/src/be/ac/umons/sgl/lazer/g06/graphic/MyButton.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/MyButton.java
@@ -1,34 +1,31 @@
 package be.ac.umons.sgl.lazer.g06.graphic;
 
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 /* Textures from packfile
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 //*/
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+/* Individual textures
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+//*/
 
 public class MyButton extends TextButton {
 	
-	public MyButton(String text) {
+	public MyButton(MySkin skin, String text) {
 		// call to super constructor with minimum style
 		super(text, new TextButtonStyle(null, null, null, new BitmapFont()));
-		
-		TextButtonStyle myButtonStyle = new TextButtonStyle();
-		// default 15pt Arial
-		myButtonStyle.font = new BitmapFont();
 		
 		/* Textures from packfile
 		TextureAtlas buttonAtlas = new TextureAtlas("relative/from/assets/to/file.pack");
 		Skin skin.addRegions(buttonAtlas);
 		//*/
-		//* Individual textures
+		/* Individual textures
 		Texture tex = new Texture("images/button.png");
-		Texture tex2 = new Texture("images/button_over.png");
 		TextureRegionDrawable drawable = new TextureRegionDrawable(new TextureRegion(tex));
-		TextureRegionDrawable drawable2 = new TextureRegionDrawable(new TextureRegion(tex2));
 		//*/
 		
 		/* Textures from packfile
@@ -37,11 +34,16 @@ public class MyButton extends TextButton {
 		myButtonStyle.checked = skin.getDrawable("checked-button");
 		//*/
 		//* Individual textures
-		myButtonStyle.up = drawable;
-		myButtonStyle.down = drawable2;
-		myButtonStyle.checked = drawable2;
+		
+		this.getStyle().up = skin.getColor(Color.DARK_GRAY);
+		this.getStyle().over = skin.getColor(Color.GRAY);
+		this.getStyle().down = skin.getColor(Color.LIGHT_GRAY);
+		
+		this.getStyle().font = skin.getFont(40, Color.WHITE, 2, Color.BLACK);
+		
 		//*/
-		super.setStyle(myButtonStyle);
+		// force style update
+		super.setStyle(this.getStyle());
 	}
 	
 }
