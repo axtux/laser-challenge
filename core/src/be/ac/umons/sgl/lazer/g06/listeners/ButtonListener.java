011267676674ad65aa2ab226f3bd86e450c1f0a6
Axtux <axtux@hotmail.com>
2017-03-03 13:35:14 +0100

    only handle click

--- core/src/be/ac/umons/sgl/lazer/g06/listeners/ButtonListener.java
+++ core/src/be/ac/umons/sgl/lazer/g06/listeners/ButtonListener.java
@@ -1,19 +1,15 @@
 package be.ac.umons.sgl.lazer.g06.listeners;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Event;
-import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
-import be.ac.umons.sgl.lazer.g06.graphic.MyButton;
-
-
-public class ButtonListener implements EventListener {
+public class ButtonListener extends ChangeListener {
 	String action;
 	
 	public ButtonListener(String action) {
 		this.action = action;
 	}
-	
+	/*
 	public boolean handle(Event event) {
 		Actor actor = event.getTarget();
 		if(! (actor instanceof MyButton) ) {
@@ -27,6 +23,11 @@ public class ButtonListener implements EventListener {
 		}
 		
 		return false;
+	}
+	//*/
+	public void changed(ChangeEvent event, Actor actor) {
+		System.out.println("doing action "+this.action);
+		
 	}
 
 }
