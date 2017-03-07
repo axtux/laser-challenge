794494fad1c40705c3b18635fee050641d19c970
Axtux <axtux@hotmail.com>
2017-03-07 08:57:36 +0100

    remove star import, remove fullscreen, set maximize

--- desktop/src/be/ac/umons/sgl/lazer/g06/desktop/DesktopLauncher.java
+++ desktop/src/be/ac/umons/sgl/lazer/g06/desktop/DesktopLauncher.java
@@ -1,17 +1,21 @@
 package be.ac.umons.sgl.lazer.g06.desktop;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import be.ac.umons.sgl.lazer.g06.graphic.*;
+
+import be.ac.umons.sgl.lazer.g06.graphic.LazerChallenge;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Lazer Challenge";
-		config.useGL30=false;
-		config.width=LazerChallenge.WIDTH;
-		config.height=LazerChallenge.HEIGHT;
-		config.fullscreen = true;
+		config.useGL30 = false;
+		//config.fullscreen = true;
 		
 		new LwjglApplication(new LazerChallenge(), config);
+		// maximize window
+		config.height = Gdx.graphics.getHeight();
+		config.width = Gdx.graphics.getWidth();
+		
 	}
 }
