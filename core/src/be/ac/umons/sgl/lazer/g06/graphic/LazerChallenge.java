0cb40ab522d71719a26eef6313451a244645d2f7
Axtux <axtux@hotmail.com>
2017-03-09 12:24:57 +0100

    better action naming

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
@@ -6,25 +6,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
-import be.ac.umons.sgl.lazer.g06.graphic.stages.ConnexionStage;
-import be.ac.umons.sgl.lazer.g06.graphic.stages.LevelSelectionStage;
-import be.ac.umons.sgl.lazer.g06.graphic.stages.ModeSelectionStage;
+import be.ac.umons.sgl.lazer.g06.graphic.stages.LoginsStage;
+import be.ac.umons.sgl.lazer.g06.graphic.stages.AbstractStage;
+import be.ac.umons.sgl.lazer.g06.graphic.stages.LevelsStage;
+import be.ac.umons.sgl.lazer.g06.graphic.stages.LocalLoginStage;
+import be.ac.umons.sgl.lazer.g06.graphic.stages.ModesStage;
+import be.ac.umons.sgl.lazer.g06.users.LocalUser;
+import be.ac.umons.sgl.lazer.g06.users.LocalUser.LoginException;
 import be.ac.umons.sgl.lazer.g06.users.User;
 
 public class LazerChallenge extends Game {
-	public static int WIDTH=1000;
-	public static int HEIGHT=720;
-	Stage stage;
+	// default to fullHD resolution
+	public static int WIDTH = 1920;
+	public static int HEIGHT = 1080;
+	
+	AbstractStage stage;
 	MySkin skin;
 	User user;
 	String mode = "";
 	
 	public void create () {
-		skin = new MySkin();
-		act("MENU_CONNECTION");
 		/*
 		Gdx.app.setLogLevel(Application.LOG_NONE);
 		//*/Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		
+		Gdx.app.debug("LOCAL_PATH", Gdx.files.getLocalStoragePath());
+		
+		skin = new MySkin();
+		act("MENU_LOGINS");
+		
 	}
 	
 	public MySkin getSkin() {
@@ -40,6 +50,10 @@ public class LazerChallenge extends Game {
 	}
 	
 	public void setUser(User user) {
+		if(this.user != null) {
+			this.user.logout();
+		}
+		
 		this.user = user;
 	}
 	
@@ -60,7 +74,7 @@ public class LazerChallenge extends Game {
 		skin.dispose();
 	}
 	
-	public void setStage(Stage newStage) {
+	public void setStage(AbstractStage newStage) {
 		if(newStage == stage) {
 			return;
 		}
@@ -68,8 +82,8 @@ public class LazerChallenge extends Game {
 		Stage oldStage = stage;
 		
 		Gdx.input.setInputProcessor(newStage);
-		this.stage = newStage;
-		this.render();
+		stage = newStage;
+		render();
 		
 		if(oldStage != null) {
 			oldStage.dispose();
@@ -80,42 +94,55 @@ public class LazerChallenge extends Game {
 		Gdx.app.debug("ACTION", action);
 		switch(action) {
 		
-		case "EXIT":
+		// ACTIONs from headers
+		case "ACTION_EXIT":
 			Gdx.app.exit();
-		case "LOGOUT":
-			logout();
-		case "MENU_CONNECTION":
-			setStage(new ConnexionStage(this));
+		case "ACTION_LOGOUT":
+			setUser(null);
+			act("MENU_LOGINS");
 			break;
 		
-		// from connection menu
-		case "CONNECTION_ANONYMOUS":
+		case "MENU_LOGINS":
+			setStage(new LoginsStage(this));
+			break;
+		// ACTIONs from MENU_LOGINS
+		case "ACTTION_LOGIN_ANONYMOUS":
 			setUser(new User());
-			act("MENU_MODE_SELECTION");
+			act("MENU_MODES");
 			break;
-		case "CONNECTION_LOCAL":
-			//setStage(new Stage(this));
+		case "ACTTION_LOGIN_LOCAL":
+			act("MENU_LOGIN_LOCAL");
 			break;
-		case "CONNECTION_TWITTER":
+		case "ACTTION_LOGIN_TWITTER":
 			//setStage(new Stage(this));
 			break;
 		
-		case "MENU_MODE_SELECTION":
-			setStage(new ModeSelectionStage(this));
+		case "MENU_LOGIN_LOCAL":
+			setStage(new LocalLoginStage(this));
 			break;
-		
-		// from mode selection
-		case "MODE_ARCADE":
+		// ACTIONs from LOGIN_LOCAL
+		case "ACTION_LOCAL_LOGIN":
+			local_user(false);
+			break;
+		case "ACTION_LOCAL_SIGNUP":
+			local_user(true);
+			break;
+			
+		case "MENU_MODES":
+			setStage(new ModesStage(this));
+			break;
+		// ACTIONs from MENU_MODES
+		case "ACTION_MODE_ARCADE":
 			mode = "ARCADE";
-			act("MENU_LEVEL_SELECTION");
+			act("MENU_LEVELS");
 			break;
-		case "MODE_TRAINING":
+		case "ACTION_MODE_TRAINING":
 			mode = "TRAINING";
-			act("MENU_LEVEL_SELECTION");
+			act("MENU_LEVELS");
 			break;
 		
-		case "MENU_LEVEL_SELECTION":
-			setStage(new LevelSelectionStage(this));
+		case "MENU_LEVELS":
+			setStage(new LevelsStage(this));
 			break;
 		
 		default:
@@ -124,12 +151,20 @@ public class LazerChallenge extends Game {
 		
 	}
 	
-	private void logout() {
-		if(user == null) {
-			return;
+	private void local_user(boolean create) {
+		String username = stage.getFieldValue("USERNAME");
+		String password = stage.getFieldValue("PASSWORD");
+		
+		try {
+			User user = new LocalUser(username, password, create);
+			setUser(user);
+			setStage(new LevelsStage(this));
+		} catch (LoginException e) {
+			setStage(new LocalLoginStage(this, username, password, e.getMessage()));
+			
+			//e.printStackTrace();
 		}
 		
-		user.logout();
-		user = null;
 	}
+	
 }
