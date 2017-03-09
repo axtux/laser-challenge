24d5f1df0c49fef588d5b3471f8abf74b8e42790
Axtux <axtux@hotmail.com>
2017-03-09 12:20:50 +0100

    easy file interface

---
+++ core/src/be/ac/umons/sgl/lazer/g06/Files.java
@@ -0,0 +1,78 @@
+package be.ac.umons.sgl.lazer.g06;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+
+public class Files {
+	/*
+	 * Get string content or null if an error occured
+	 */
+	public static String getContent(String path) {
+		FileHandle fh = Gdx.files.local(path);
+		
+		if(fh == null) {
+			Gdx.app.debug("Files.getContent", "Got null pointer for path "+path);
+			return null;
+		}
+
+		if(fh.isDirectory()) {
+			Gdx.app.debug("Files.getContent", path+" is a directory");
+			return null;
+		}
+		
+		String content;
+		try {
+			content = fh.readString("UTF-8");
+		} catch(GdxRuntimeException e) {
+			Gdx.app.debug("Files.getContent", "Error reading "+path+" : "+e.getMessage());
+			return null;
+		}
+		
+		return content;
+	}
+
+	public static boolean putContent(String path, String content) {
+		return putContent(path, content, false);
+	}
+	
+	public static boolean putContent(String path, String content, boolean append) {
+		FileHandle fh = Gdx.files.local(path);
+		
+		if(fh == null) {
+			Gdx.app.debug("Files.putContent", "Got null pointer for path "+path);
+			return false;
+		}
+		
+		if(fh.isDirectory()) {
+			Gdx.app.debug("Files.putContent", path+" is a directory");
+			return false;
+		}
+		
+		try {
+			fh.writeString(content, append, "UTF-8");
+		} catch(GdxRuntimeException e) {
+			Gdx.app.debug("Files.putContent", "Error writing "+path+" : "+e.getMessage());
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean exists(String path) {
+		FileHandle fh = Gdx.files.local(path);
+		
+		if(fh == null) {
+			Gdx.app.debug("Files.exists", "Got null pointer for path "+path);
+			return false;
+		}
+		
+		if(fh.isDirectory()) {
+			Gdx.app.debug("Files.exists", path+" is a directory");
+			return false;
+		}
+		
+		return fh.exists();
+	}
+	
+}
