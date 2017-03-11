f7acba8fe21c1d5bfebc5ea59b2d8dd23f9cdad2
Axtux <axtux@hotmail.com>
2017-03-11 14:49:11 +0100

    add level selection

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/stages/LevelsStage.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/stages/LevelsStage.java
@@ -1,15 +1,84 @@
 package be.ac.umons.sgl.lazer.g06.graphic.stages;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
 import be.ac.umons.sgl.lazer.g06.graphic.LazerChallenge;
+import be.ac.umons.sgl.lazer.g06.listeners.MyClickListener;
 
 public class LevelsStage extends AbstractStage {
 	
+	String mode;
+	boolean scores;
+	
 	public LevelsStage(LazerChallenge game) {
 		super(game, "Mode "+game.getMode()+" : Choix du niveau", "MENU_MODES");
+		setMode(game.getMode());
 		
-		addMenuButton("Level 1", "ACTION_LEVEL_1");
-		addMenuButton("Level 2", "ACTION_LEVEL_2");
-		addMenuButton("Level 3", "ACTION_LEVEL_3");
+		addLevelsBlock("Niveaux standards");
+		addLevelsBlock("Niveaux avancés");
+	}
+	
+	private boolean setMode(String mode) {
+		scores = false;
+		
+		switch(mode) {
+		case "ARCADE":
+			scores = true;
+		case "TRAINING":
+			this.mode = mode;
+			return true;
+		default:
+			Gdx.app.error("LevelsStage.setMode", "mode "+mode+" not implemented");
+			return false;
+		}
+	}
+	
+	private void addLevelsBlock(String name) {
+		Table block = new Table();
+		
+		Label title = new Label(name+" : ", skin, "label");
+		block.row();
+		block.add(title).pad(20).padLeft(100).left();
+		
+		Table levelsContainer = new Table();
+		for(int i = 1; i <= 10; ++i) {
+			addLevelButton(levelsContainer, i, false);
+		}
+		
+		block.row();
+		block.add(levelsContainer).expand().top();
+		
+		content.row();
+		content.add(block).expandX().fillX().top().left();
+	}
+	
+	protected void addLevelButton(Table container, int number, boolean locked) {
+		String str_number = Integer.toString(number);
+		Table levelContainer = new Table();
+		
+		TextButton btn = new TextButton(str_number, skin, locked ? "disabled-menu" : "menu");
+		btn.getLabelCell().pad(10);
+		if(!locked) {
+			btn.addListener(new MyClickListener(game, Input.Buttons.LEFT, "ACTION_LEVEL_"+mode+"_"+str_number));
+		}
+		levelContainer.add(btn).minSize(80, 80).pad(10);
+		levelContainer.row();
+		
+		Label subtitle = new Label("Niveau "+str_number, skin, "small-label");
+		levelContainer.add(subtitle);
+		levelContainer.row();
+		
+		if(scores) {
+			Label score = new Label("Score : 0", skin, "small-label");
+			levelContainer.add(score);
+		}
+		
+		container.add(levelContainer).pad(20);
+		levelContainer.debug();
 	}
 
 }
