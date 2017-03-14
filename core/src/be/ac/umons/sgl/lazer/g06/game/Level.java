d6741055ee70ae540c69f82e55b2ff87f90918f0
Axtux <axtux@hotmail.com>
2017-03-14 08:48:59 +0100

    a bit more on Level

--- core/src/be/ac/umons/sgl/lazer/g06/game/Level.java
+++ core/src/be/ac/umons/sgl/lazer/g06/game/Level.java
@@ -1,11 +1,19 @@
 package be.ac.umons.sgl.lazer.g06.game;
 
+import com.badlogic.gdx.Gdx;
+
+import be.ac.umons.sgl.lazer.g06.Files;
+
 public class Level {
 	Mode mode;
-	Type type;
+	String type;
 	int number;
-	
-	public Level(Mode mode, Type type, int number) {
+
+	public Level(String file) {
+		Gdx.app.debug("Level.Level", "creating level from file "+file);
+	}
+		
+	public Level(Mode mode, String type, int number) {
 		this.mode = mode;
 		this.type = type;
 		this.number = number;
@@ -14,12 +22,20 @@ public class Level {
 	public enum Mode {
 		ARCADE, TRAINING
 	}
-	public enum Type {
-		STANDARD, ADVANCED
-	}
 	
-	public static Level[] listLevels() {
+	public static Level[] listLevels(String type) {
+		String levelsDir = "levels/"+type.toLowerCase();
+		String levelFiles[] = Files.list(levelsDir);
 		
+		if(levelFiles == null) {
+			Gdx.app.error("Level.listLevels", "Got null while listing directory "+levelsDir);
+			return null;
+		}
+		
+		Level levels[] = new Level[levelFiles.length];
+		for(int i = 0; i < levelFiles.length; ++i) {
+			levels[i] = new Level(levelFiles[i]);
+		}
 		return null;
 	}
 }
