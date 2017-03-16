c5314f81155cfcfbbfbd2fd7cde043b431f2ae10
MaazouzMehdi <skyrim1995@hotmail.fr>
2017-03-16 18:15:40 +0100

    new directories and modification of map for the level 1

---
+++ core/src/be/ac/umons/sgl/lazer/g06/game/ParserLevel.java
@@ -0,0 +1,14 @@
+package be.ac.umons.sgl.lazer.g06.game;
+
+public class ParserLevel {
+	
+	private String name;
+	private String grille;
+	private String difficulty;
+	private  size;
+	
+	public ParserLevel {
+		
+	}
+
+}
