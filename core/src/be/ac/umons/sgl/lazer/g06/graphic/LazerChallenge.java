318bb60aa25837f3fe5b47820b36de2a74ac07ba
Axtux <axtux@hotmail.com>
2017-03-19 21:01:10 +0100

    add javadoc

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/LazerChallenge.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 
 import be.ac.umons.sgl.lazer.g06.game.Level;
 import be.ac.umons.sgl.lazer.g06.graphic.stages.AbstractStage;
@@ -12,6 +13,9 @@ import be.ac.umons.sgl.lazer.g06.listeners.ActionListener;
 import be.ac.umons.sgl.lazer.g06.listeners.MyActionListener;
 import be.ac.umons.sgl.lazer.g06.users.User;
 
+/**
+ * Controller of the game
+ */
 public class LazerChallenge extends Game {
 	// default to fullHD resolution
 	public static int WIDTH = 1920;
@@ -21,21 +25,31 @@ public class LazerChallenge extends Game {
 	// perform actions from UI, pattern strategy
 	ActionListener al;
 	
-	AbstractStage stage;
 	MySkin skin;
+	AbstractStage stage;
 	User user;
 	Level.Mode mode;
 	Level level;
-	
+	/**
+	 * Check single instance and sage it statically.
+	 * Initialize action listener.
+	 */
 	public LazerChallenge() {
+		if(instance != null) {
+			throw new GdxRuntimeException("Singleton pattern does not allow multiple intances.");
+		}
 		LazerChallenge.instance = this;
 		al = new MyActionListener(this);
 	}
-	
+	/**
+	 * @return Singleton pattern instance.
+	 */
 	public static LazerChallenge getInstance() {
 		return instance;
 	}
-	
+	/**
+	 * Set debug values, create application skin and display login menu.
+	 */
 	public void create () {
 		/*
 		Gdx.app.setLogLevel(Application.LOG_NONE);
@@ -48,15 +62,27 @@ public class LazerChallenge extends Game {
 		//act("ACTION_LEVEL_PLAY");
 		
 	}
-	
+	/**
+	 * @return Skin created by constructor and containing all UI @Drawable
+	 */
 	public MySkin getSkin() {
 		return skin;
 	}
-
+	/**
+	 * @return Current stage displayed on screen.
+	 */
+	public AbstractStage getStage() {
+		return stage;
+	}
+	/**
+	 * @return Current user playing game.
+	 */
 	public User getUser() {
 		return user;
 	}
-	
+	/**
+	 * @return Mode selected by user.
+	 */
 	public Level.Mode getMode() {
 		return mode;
 	}
@@ -67,11 +93,16 @@ public class LazerChallenge extends Game {
 	public void setMode(Level.Mode mode) {
 		this.mode = mode;
 	}
-	
+	/**
+	 * @return Current level being played.
+	 */
 	public Level getLevel() {
 		return level;
 	}
-	
+	/**
+	 * Log out old user (if it exists) and set new user.
+	 * @param user New user
+	 */
 	public void setUser(User user) {
 		if(this.user != null) {
 			this.user.logout();
@@ -79,15 +110,22 @@ public class LazerChallenge extends Game {
 		
 		this.user = user;
 	}
-	
+	/**
+	 * Change current level
+	 * @param level New level
+	 */
 	public void setLevel(Level level) {
 		this.level = level;
 	}
-	
+	/**
+	 * Called when window is resized. Ask stage viewport to resize.
+	 */
 	public void resize (int width, int height) {
 		stage.getViewport().update(width, height, true);
 	}
-	
+	/**
+	 * Clear screen and draw stage.
+	 */
 	public void render() {
 		super.render();
 		Gdx.gl.glClearColor(0.55f, 0.55f, 0.55f, 1f);
@@ -95,14 +133,18 @@ public class LazerChallenge extends Game {
 		
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
-		
 	}
-	
+	/**
+	 * Free resources
+	 */
 	public void dispose() {
 		stage.dispose();
 		skin.dispose();
 	}
-	
+	/**
+	 * Free resources of old stage (if exists) and set new stage.
+	 * @param newStage New Stage
+	 */
 	public void setStage(AbstractStage newStage) {
 		if(newStage == stage) {
 			return;
@@ -119,11 +161,10 @@ public class LazerChallenge extends Game {
 		}
 		
 	}
-	
-	public AbstractStage getStage() {
-		return stage;
-	}
-	
+	/**
+	 * Called by UI elements to make action
+	 * @param action String representation of an action.
+	 */
 	public void act(String action) {
 		al.act(action);
 	}
