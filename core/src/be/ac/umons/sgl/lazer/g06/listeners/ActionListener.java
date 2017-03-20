b4647fcee3f554a5abef1a94323c97a7ec7674f7
Axtux <axtux@hotmail.com>
2017-03-20 01:03:06 +0100

    fix javadoc

--- core/src/be/ac/umons/sgl/lazer/g06/listeners/ActionListener.java
+++ core/src/be/ac/umons/sgl/lazer/g06/listeners/ActionListener.java
@@ -5,6 +5,7 @@ package be.ac.umons.sgl.lazer.g06.listeners;
 public interface ActionListener {
 	/**
 	 * Called by LaserChallenge.act(String)
+	 * @param action Action to perform.
 	 */
 	public void act(String action);
 }
