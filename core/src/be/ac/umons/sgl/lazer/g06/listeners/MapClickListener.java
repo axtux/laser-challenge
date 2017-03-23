3a19a2d5f84fa28b014b99b73b5f891243d4ee58
Axtux <axtux@hotmail.com>
2017-03-23 18:14:03 +0100

    rename and set real rotate action

--- core/src/be/ac/umons/sgl/lazer/g06/listeners/MapClickListener.java
+++ core/src/be/ac/umons/sgl/lazer/g06/listeners/MapClickListener.java
@@ -1,13 +1,12 @@
 package be.ac.umons.sgl.lazer.g06.listeners;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import be.ac.umons.sgl.lazer.g06.game.Position;
 /**
  * Listener to keep track of tile clicks.
  */
-public class TileClickListener extends MyClickListener {
+public class MapClickListener extends MyClickListener {
 	Position position;
 	/**
 	 * Create listener.
@@ -15,21 +14,18 @@ public class TileClickListener extends MyClickListener {
 	 * @param position Position of tile.
 	 * @param action Action to send to game when mouseButton is clicked.
 	 */
-	public TileClickListener(int mouseButton, Position position, String action) {
+	public MapClickListener(int mouseButton, Position position, String action) {
 		super(mouseButton, action);
 		this.position = position;
 	}
 	
 	public void clicked(InputEvent event, float x, float y) {
-		String pos = position.getLocation().toString()+" : "+Integer.toString(position.getX())+"x"+Integer.toString(position.getY());
-		Gdx.app.debug("TileClickListener from "+pos, "ACTION : "+action);
-		
-		Actor actor = event.getListenerActor();
+		Gdx.app.debug("MapClickListener from "+position.toString(), "ACTION : "+action);
 		
 		switch(action) {
 		case "ACTION_LEVEL_TILE_ROTATE":
-			Gdx.app.debug("TileClickListener.clicked", "Rotating tile");
-			actor.rotateBy(90);
+			Gdx.app.debug("MapClickListener.clicked", "Rotating tile");
+			game.getLevel().getMap().rotate(position);
 			break;
 		}
 	}
