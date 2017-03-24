be6d2c14d56575eb21b597d6c77e06a0447f42e2
Axtux <axtux@hotmail.com>
2017-03-24 02:01:51 +0100

    enhance playing UI

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/stages/LevelPlayingStage.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/stages/LevelPlayingStage.java
@@ -1,6 +1,7 @@
 package be.ac.umons.sgl.lazer.g06.graphic.stages;
 
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
@@ -25,17 +26,19 @@ public class LevelPlayingStage extends AbstractLevelStage {
 		content.row();
 		
 		leftTable = new Table();
-		content.add(new ScrollPane(leftTable, skin)).pad(10).fill().uniform();
+		content.add(new ScrollPane(leftTable, skin, "gray")).pad(10).fill().uniform();
 		addLegend(leftTable);
 		
 		mapTable = new Table();
 		content.add(mapTable).grow();
+		//addInventory(mapTable, level.getInventory());
 		addMapTable(mapTable, level.getMap());
 		
 		rightTable = new Table();
 		content.add(new ScrollPane(rightTable, skin)).pad(10).fill().uniform();
-		addInventory(rightTable, level.getInventory());
 		addInfos(rightTable);
+		addInventory(rightTable, level.getInventory());
+		addSelection(rightTable);
 	}
 	
 	private void addLegend(Table container) {
@@ -64,18 +67,51 @@ public class LevelPlayingStage extends AbstractLevelStage {
 	
 	private void addMapTable(Table container, Map map) {
 		Table mapTable = new MapTable(map);
+		container.row();
 		container.add(mapTable).grow().pad(20);
 	}
 	
-	private void addInventory(Table container, Map inv) {
+	private Table addBox(Table container, String title) {
+		Table box = new Table();
+		box.background(skin.getColor(Color.LIGHT_GRAY));
 		container.row();
-		Table mapTable = new MapTable(inv);
-		container.add(mapTable).grow().pad(10);
+		container.add(box).growX().padBottom(10);
+		
+		Label label = new Label(title, skin, "label");
+		box.row();
+		box.add(label).pad(20);
+		
+		Table content = new Table();
+		content.background(skin.getColor(Color.WHITE));
+		box.row();
+		box.add(content).pad(20).padTop(0);
+		
+		return content;
 	}
 	
 	private void addInfos(Table container) {
-		container.row();
-		addButton(container, "Swap", "ACTION_LEVEL_SWAP", "small-menu").pad(20);
+		Table content = addBox(container, "Informations");
+		addDoubleLabel(content, "Temps", "XX:XX", "small-label");
+		addDoubleLabel(content, "Score", "XXX", "small-label");
+	}
+	
+	private void addInventory(Table container, Map inv) {
+		Table content = addBox(container, "Inventaire");
+		Table mapTable = new MapTable(inv);
+		content.row();
+		content.add(mapTable).pad(5);
+	}
+	
+	private void addSelection(Table container) {
+		Table content = addBox(container, "Sélection");
+		addDoubleLabel(content, "Block", "source", "small-label");
+		
+		addDoubleLabel(content, "Déplacer", "autorisé", "small-label");
+		addDoubleLabel(content, "Orienter", "autorisé", "small-label");
+		
+		content.row();
+		addButton(content, "Swap", "ACTION_LEVEL_SWAP", "small-menu").pad(20).colspan(2);
+		
 		container.row();
 		addButton(container, "Lancer le lazer", "ACTION_LEVEL_LASER", "small-menu").pad(20);
 	}
