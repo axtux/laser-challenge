87db8c757d9a68609f10c26868c0ba869a6dec04
Axtux <axtux@hotmail.com>
2017-03-27 14:05:02 +0200

    add laser input

--- core/src/be/ac/umons/sgl/lazer/g06/game/Block.java
+++ core/src/be/ac/umons/sgl/lazer/g06/game/Block.java
@@ -2,11 +2,16 @@ package be.ac.umons.sgl.lazer.g06.game;
 
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
+import com.badlogic.gdx.utils.Array;
 
 import be.ac.umons.sgl.lazer.g06.game.LevelType.BlockType;
+import be.ac.umons.sgl.lazer.g06.game.LevelType.Orientation;
 
 public class Block extends TiledMapTileLayer.Cell {
 	BlockType type;
+	// for laser
+	Orientation input;
+	boolean processed;
 	
 	public Block(BlockType type) {
 		super();
@@ -14,7 +19,33 @@ public class Block extends TiledMapTileLayer.Cell {
 		setTile(new StaticTiledMapTile(type.getTextureRegion()));
 	}
 	
+	public boolean input(Orientation orientation) {
+		if(processed) {
+			return false;
+		}
+		
+		processed = true;
+		input = orientation;
+		return true;
+	}
+	
+	public Orientation getInput() {
+		return input;
+	}
+	
+	public Array<Orientation> getOutputs() {
+		if(!processed) {
+			return new Array<Orientation>(0);
+		}
+		return type.input(input);
+	}
+	
 	public BlockType getType() {
 		return type;
+	}
+	
+	public void clearInput() {
+		input = null;
+		processed = false;
 	}
 }
