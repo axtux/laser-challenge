dbbf085bffbc7258672cb12207555d135e6320d9
Axtux <axtux@hotmail.com>
2017-03-27 14:49:01 +0200

    add laser drawing

--- core/src/be/ac/umons/sgl/lazer/g06/graphic/MapButton.java
+++ core/src/be/ac/umons/sgl/lazer/g06/graphic/MapButton.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Array;
 
+import be.ac.umons.sgl.lazer.g06.game.Block;
+import be.ac.umons.sgl.lazer.g06.game.LevelType.BlockType;
+import be.ac.umons.sgl.lazer.g06.game.LevelType.Orientation;
 import be.ac.umons.sgl.lazer.g06.game.Map;
 import be.ac.umons.sgl.lazer.g06.game.Position;
 import be.ac.umons.sgl.lazer.g06.listeners.MapClickListener;
@@ -18,6 +21,9 @@ public class MapButton extends Button {
 	Map map;
 	Position pos;
 	Array<Cell> cells;
+	Block block;
+	
+	TextureRegion inputTR, outputTR;
 	
 	public MapButton(Map map, Position pos) {
 		super();
@@ -27,12 +33,16 @@ public class MapButton extends Button {
 		this.addListener(new MapClickListener(Input.Buttons.LEFT, pos, "ACTION_LEVEL_SELECT"));
 		this.addListener(new MapClickListener(Input.Buttons.RIGHT, pos, "ACTION_LEVEL_ROTATE"));
 		
+		inputTR = LazerChallenge.getInstance().getLevel().getType().getInput();
+		outputTR = LazerChallenge.getInstance().getLevel().getType().getOutput();
+		
 		update();
 		setBackground();
 	}
 	
 	public void update() {
 		cells = map.getCells(pos);
+		block = map.getBlock(pos);
 		Gdx.app.debug("TileButton.update", "called for position "+pos.toString());
 	}
 	
@@ -51,9 +61,30 @@ public class MapButton extends Button {
 	public void draw(Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
 		
-		TextureRegion tr;
-		int angle = 0;
+		if(block == null) {
+			return;
+		}
 		
+		// display block
+		draw(batch, block.getTile().getTextureRegion(), block.getRotation());
+		
+		// stop if laser not on block
+		if(!block.processed()) {
+			return;
+		}
+		
+		Orientation input = block.getInput();
+		if(input != null) {
+			draw(batch, inputTR, input.getAngle());
+		}
+		
+		for(Orientation output : block.getOutputs()) {
+			draw(batch, outputTR, output.getAngle());
+		}
+		
+		
+		
+		/*
 		// don't draw background as already displayed by button
 		//for(Cell cell : cells) {
 		for(int i = 1; i < cells.size; ++i) { Cell cell = cells.get(i);
@@ -61,5 +92,10 @@ public class MapButton extends Button {
 			angle = cell.getRotation();
 			batch.draw(tr, getX(), getY(), getWidth()/2, getHeight()/2, getWidth(), getHeight(), 1, 1, -angle);
 		}
+		//*/
+	}
+	
+	private void draw(Batch batch, TextureRegion tr, int rotation) {
+		batch.draw(tr, getX(), getY(), getWidth()/2, getHeight()/2, getWidth(), getHeight(), 1, 1, -rotation);
 	}
 }
