c3f6bc6ddec7ad6b82502acb31a8a39abdbfb9f9
Axtux <axtux@hotmail.com>
2017-03-28 19:08:16 +0200

    respect initial conventions

--- core/src/be/ac/umons/sgl/lazer/g06/listeners/MyActionListener.java
+++ core/src/be/ac/umons/sgl/lazer/g06/listeners/MyActionListener.java
@@ -70,10 +70,10 @@ public class MyActionListener implements ActionListener {
 			break;
 		// ACTIONs from LOGIN_LOCAL
 		case "ACTION_LOCAL_LOGIN":
-			local_user(false);
+			localUser(false);
 			break;
 		case "ACTION_LOCAL_SIGNUP":
-			local_user(true);
+			localUser(true);
 			break;
 			
 		case "MENU_MODES":
@@ -134,7 +134,7 @@ public class MyActionListener implements ActionListener {
 	 * Manage local user connection.
 	 * @param create If true, user will be created. If false, user will be logged in.
 	 */
-	private void local_user(boolean create) {
+	private void localUser(boolean create) {
 		String username = game.getStage().getFieldValue("USERNAME");
 		String password = game.getStage().getFieldValue("PASSWORD");
 		
