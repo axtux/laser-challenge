70b45279853470fbd339857dc8a457a98340c60c
Axtux <axtux@hotmail.com>
2017-03-29 13:28:37 +0200

    remove TODO

--- core/src/be/ac/umons/sgl/lazer/g06/Security.java
+++ core/src/be/ac/umons/sgl/lazer/g06/Security.java
@@ -10,7 +10,8 @@ public class Security {
 	/**
 	 * md5 Hash calculation of data string.
 	 * @param data Data to hash.
-	 * @return MD5 hash lower cased string representing of the data string
+	 * @return MD5 hash lower cased string representing of the data string.
+	 * Return null if md5 or UTF-8 is not supported by Java implementation.
 	 */
 	public static String md5(String data) {
 		MessageDigest md5;
@@ -19,13 +20,9 @@ public class Security {
 			md5 = MessageDigest.getInstance("md5");
 			hash = md5.digest(data.getBytes("UTF-8"));
 		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "";
+			return null;
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return "";
+			return null;
 		}
 		
 		return javax.xml.bind.DatatypeConverter.printHexBinary(hash).toLowerCase();
