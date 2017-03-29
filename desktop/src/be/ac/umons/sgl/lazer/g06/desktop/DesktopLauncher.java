95df4f9c0527854ed633ac3776aaf504d21ae48e
Axtux <axtux@hotmail.com>
2017-03-29 16:39:06 +0200

    remove unused import, useGL3

--- desktop/src/be/ac/umons/sgl/lazer/g06/desktop/DesktopLauncher.java
+++ desktop/src/be/ac/umons/sgl/lazer/g06/desktop/DesktopLauncher.java
@@ -2,7 +2,6 @@ package be.ac.umons.sgl.lazer.g06.desktop;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.badlogic.gdx.utils.Timer;
 
 import be.ac.umons.sgl.lazer.g06.game.LazerChallenge;
 
@@ -10,8 +9,6 @@ public class DesktopLauncher {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Lazer Challenge";
-		config.useGL30 = false;
-		
 		config.width = LazerChallenge.WIDTH;
 		config.height = LazerChallenge.HEIGHT;
 		//*
