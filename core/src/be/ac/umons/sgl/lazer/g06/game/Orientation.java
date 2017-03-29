34f9bcdb7e1ed18ee8f1047ffacadb7bb9ff9c91
Axtux <axtux@hotmail.com>
2017-03-29 21:05:25 +0200

    add advanced orientations into Orientation

--- core/src/be/ac/umons/sgl/lazer/g06/game/Orientation.java
+++ core/src/be/ac/umons/sgl/lazer/g06/game/Orientation.java
@@ -1,10 +1,22 @@
 package be.ac.umons.sgl.lazer.g06.game;
 
 public enum Orientation {
+	//* Standard orientations
 	UP(0, 1),
 	RIGHT(1, 0),
 	DOWN(0, -1),
 	LEFT(-1, 0);
+	//*/
+	/* Advanced orientations
+	UP(0, 1),
+	UP_RIGHT(1, 1),
+	RIGHT(1, 0),
+	DOWN_RIGHT(1, -1),
+	DOWN(0, -1),
+	DOWN_LEFT(-1, -1),
+	LEFT(-1, 0),
+	UP_LEFT(-1, 1);
+	//*/
 	
 	int x, y;
 	private Orientation(int x, int y) {
